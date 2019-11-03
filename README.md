e26408b90cb55af8112343b824d4fbf2d6c21659
Scott Mandrell <sdmandrell@gmail.com>
2019-11-03 15:51:50 -0800

    added to readme

---
+++ README.md
@@ -0,0 +1,3 @@
+### Running scrapy project
+`cd case_collector`
+`scrapy crawl record_scraper`
--- case_collector/case_collector/spiders/record_scraper.py
+++ case_collector/case_collector/spiders/record_scraper.py
@@ -35,25 +35,3 @@ class CourtRecordScraper(scrapy.Spider):
         df = pd.read_html(str(table))
         dfs.append(df)
         print(dfs)
-
-        # self.scrape_data(self.driver.page_source)
-
-    #     return scrapy.FormRequest.from_response(
-    #         response,
-    #         formname="form1",
-    #         formdata={"txtCaseNumber": SEARCH_TEXT,
-    #                   '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first(),
-    #         '__VIEWSTATEGENERATOR': response.css('input#__VIEWSTATEGENERATOR::attr(value)').extract_first()},
-    #         callback=self.scrape_data
-    #     )
-    #
-    # def scrape_data(self, response):
-    #     b_soup = BeautifulSoup(response.body, "lxml")
-    #     tables = b_soup.find_all('table')
-    #     if not tables:
-    #         return
-    #     table = tables[0]
-    #     df = pd.read_html(str(table))
-    #     dfs.append(df)
-    #     print("HEREE")
-    #     print(dfs)
